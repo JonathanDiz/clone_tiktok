4ddcec7a9c53736093e952e6b0a8442905ca5f53
JonathanDiz <you@example.com>
2023-01-28 13:59:22 -0300

    Icons FeedVideo + estilos css

---
+++ clone_tiktok/src/components/FeedVideos/index.jsx
@@ -0,0 +1,95 @@
+import VideoPlayer from "../VideoPlayer";
+import styles from "./styles.module.css";
+
+const VIDEOS = [
+  {
+    id: 1,
+    author: "misuegraenpijama",
+    description: "es hora de la cena",
+    likes: "3000",
+    shares: "333",
+    songTitle: "sonido de suegra bellaca",
+    albumCover: "suegra2.0",
+    src: "../../../assets/08ed73aca7b91254f0e7179895d526dd.mp4",
+  },
+  {
+    id: 2,
+    author: "misuegraenpijama",
+    description: "es hora de la cena",
+    likes: "3000",
+    shares: "333",
+    songTitle: "la suegra contraataca",
+    albumCover: "suegra2.0",
+    src: "../../../assets/0a019cc20b7338572078f2e0d3f7ad3c.mp4",
+  },
+  {
+    id: 3,
+    author: "misuegraenpijama",
+    description: "es hora de la cena",
+    likes: "3000",
+    shares: "333",
+    songTitle: "suegra revenge",
+    albumCover: "suegra2.0",
+    src: "../../../assets/2717eb2a0851990b174e9f23bf500328.mp4",
+  },
+  {
+    id: 4,
+    author: "misuegraenpijama",
+    description: "es hora de la cena",
+    likes: "3000",
+    shares: "333",
+    songTitle: "suegra reload",
+    albumCover: "suegra2.0",
+    src: "../../../assets/31f8ea8011e63c308f742534673f8d4e.mp4",
+  },
+  {
+    id: 5,
+    author: "misuegraenpijama",
+    description: "es hora de la cena",
+    likes: "3000",
+    shares: "333",
+    songTitle: "suegra fuera de la matrix",
+    albumCover: "suegra2.0",
+    src: "../../../assets/5153099b32b1544e3aed9805e3d75446.mp4",
+  },
+  {
+    id: 6,
+    author: "misuegraenpijama",
+    description: "es hora de la cena",
+    likes: "3000",
+    shares: "333",
+    songTitle: "suegra interestelar",
+    albumCover: "suegra2.0",
+    src: "../../../assets/8dc229b07a6e5c43ed3f4c68bc3fa6fb.mp4",
+  },
+  {
+    id: 7,
+    author: "misuegraenpijama",
+    description: "es hora de la cena",
+    likes: "3000",
+    shares: "333",
+    songTitle: "suegra en pijama",
+    albumCover: "suegra2.0",
+    src: "../../../assets/d348c968d5fbead7e6eef9352ddd9d79.mp4",
+  },
+  {
+    id: 8,
+    author: "misuegraenpijama",
+    description: "es hora de la cena",
+    likes: "3000",
+    shares: "333",
+    songTitle: "lo que callan las suegras",
+    albumCover: "suegra2.0",
+    src: "../../../assets/d387985fadd08625dc20be675b7bf0ce.mp4",
+  },
+];
+
+export default function FeedVideos() {
+  return VIDEOS.map((video) => {
+    return (
+      <div key={video.id} className={styles.item}>
+        <VideoPlayer {...video} />
+      </div>
+    );
+  });
+}
--- clone_tiktok/src/components/VideoPlayer/index.jsx
+++ clone_tiktok/src/components/VideoPlayer/index.jsx
@@ -2,32 +2,30 @@ import { useRef, useState } from "react";
 import styles from "./styles.module.css";
 import clsx from "clsx";
 
-export default function VideoPlayer() {
+export default function VideoPlayer({ src }) {
   const [playing, setPlaying] = useState(false);
   const video = useRef(null);
 
   const handlePlay = () => {
-    const { current: videoEl } = video
-    playing
-    ? videoEl.pause()
-    : videoEl.play()
+    const { current: videoEl } = video;
+    playing ? videoEl.pause() : videoEl.play();
 
     setPlaying(!playing);
   };
 
   const playerClassName = clsx(styles.player, {
-    [styles.hidden]: playing
-  })
+    [styles.hidden]: playing,
+  });
 
   return (
-    <div>
+    <div className={styles.wrapper}>
       <video
         className={styles.video}
-        src={src}
-        loop
         controls={false}
-        ref={video}
+        loop
         onClick={handlePlay}
+        ref={video}
+        src={src}
       />
       <i className={playerClassName} onClick={handlePlay} />
     </div>
